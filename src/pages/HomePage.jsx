2a697536d0addde2f9844b96a3adf6df66b8f502
lord-amresh <amresh2006@gmail.com>
2025-05-28 08:50:31 +0545

    day 22 complete

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,28 @@
+import Card from "../components/Card"
 import Navbar from "../components/Navbar"
 
 
 function HomePage(){
     return(
-        <>
+         <>
         <Navbar/>
+        <div className="flex flex-wrap">
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+        </div>
 
 
-        </>
+         </>
     )
 }
 
