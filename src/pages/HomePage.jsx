a52c2156977f49a71ebcd4c33177099bcbfa9727
lord-amresh <amresh2006@gmail.com>
2025-05-30 16:47:14 +0545

    add home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,27 +1,31 @@
+import { useEffect, useState } from "react"
 import Card from "../components/Card"
 import Navbar from "../components/Navbar"
-
+import axios from "axios"
 
 function HomePage(){
+    const [books,setBooks] = useState([])
+    const fetchBooks = async ()=>{
+        const response = await axios.get("http://localhost:3000/api/books/")
+        setBooks(response.data.datas)
+    }
+    useEffect(()=>{
+        fetchBooks()
+    },[])
+
     return(
          <>
         <Navbar/>
         <div className="flex flex-wrap">
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
+            {
+                books.map(function(book){//books bhanne state lai loop gariyo using map
+                    return (
+                        <Card book = {book} />
+                    )
+                })
+            }
+                     
         </div>
-
-
          </>
     )
 }
