c6a995b1423ed8bbc748d706b8c8c41016167a50
lord-amresh <amresh2006@gmail.com>
2025-06-01 15:34:44 +0545

    single page , delete page bhayo..

--- src/pages/SinglePage.jsx
+++ src/pages/SinglePage.jsx
@@ -1,10 +1,11 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
 
 function SinglePage(){
+    const navigate = useNavigate()
     const {id} = useParams()  //used to get the parameter of route..
     const [book,setBook] = useState({})
     const fetchBook = async ()=>{
@@ -14,12 +15,24 @@ function SinglePage(){
     useEffect(()=>{
         fetchBook()
     },[])
+
+    const deleteBook = async ()=>{
+        const response = await axios.delete("http://localhost:3000/api/books/" + id)
+        if(response.status === 200){
+            // Home page ma navigation gardim
+            navigate("/")
+        }else{
+            alert("Something went Wrong")
+        }
+    }
+
     return(
         <>
         <Navbar />
         <h1>{book.bookName}</h1>
         <p>{book.bookPrice}</p>
         <p>{book.bookAuthor}</p>
+        <button onClick={deleteBook}>Delete Me</button>
         </>
     )
 }
